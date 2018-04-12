80dd1ab9d25d6f821a6e6c8cc426bb353e12fb27
anthony <anthonyqchao@gmai..com>
2018-04-12 00:48:48 -0700

    first FT, using unittest library now

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox(executable_path="/mnt/c/Users/Anthony/AppData/Local/Programs/Python/Python36/Scripts/geckodriver.exe")
-browser.get('http://localhost:8000')
+path = "/mnt/c/Users/Anthony/AppData/Local/Programs/Python/Python36/Scripts/geckodriver.exe"
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path=path)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
