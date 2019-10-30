ec81e929451a643512939cca325cfbbefda0063e
anthonysea <anthonyqchao@gmail.com>
2019-10-30 14:02:34 -0700

    Added helper function to functional_tests to check if item in table; added unit test to check it the app can save a POST request

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     # Any method whose name starts with test is a test method, and will be run by the test runner
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Users goes to check out to-do list homepage
@@ -39,20 +44,21 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for rows in rows),
-            "New to-do item did not appear in table"
-        )
-
         # There is still a text box inviting the user to add another item. She
         # enters "Use peacock feathers to make a fly"
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on the user's list
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # The page has generated a unique URL for the user 
+        self.fail("Finish the test!")
 
         # User visits that URL, and the to-do list is still there
 
